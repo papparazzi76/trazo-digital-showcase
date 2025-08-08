0ea8554e391dc74753f2ab981f9868f8ef5491d4
papparazzi76 <carlos@arcasl.es>
2025-08-08 12:54:50 +0200

    Create SchemaOrg.tsx

---
+++ src/components/SchemaOrg.tsx
@@ -0,0 +1,24 @@
+import { Helmet } from 'react-helmet-async';
+
+type SchemaType = 'Article' | 'Service' | 'Blog';
+
+interface SchemaOrgProps {
+  schema: object;
+  type: SchemaType;
+}
+
+const SchemaOrg = ({ schema, type }: SchemaOrgProps) => {
+  const schemaWithType = {
+    '@context': 'https://schema.org',
+    '@type': type,
+    ...schema,
+  };
+
+  return (
+    <Helmet>
+      <script type="application/ld+json">{JSON.stringify(schemaWithType)}</script>
+    </Helmet>
+  );
+};
+
+export default SchemaOrg;
